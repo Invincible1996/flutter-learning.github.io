0acd6755eda6ff92a531b7c8cd7d837e80073ef6
wenqiang.zhang <717092390@qq.com>
2021-09-17 15:23:03 +0800

    Updated docs

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "d7cd0ef48f0fd2eae12d57bdb0806401",
-"index.html": "296185386653605e3f648ecdb4f20973",
-"/": "296185386653605e3f648ecdb4f20973",
+"index.html": "2e902ff6be9752416a26bd81065bf90b",
+"/": "2e902ff6be9752416a26bd81065bf90b",
 "main.dart.js": "e23c0b736356984c3455e7dc0174b47a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
